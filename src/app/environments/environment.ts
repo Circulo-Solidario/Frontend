3fe58a3a63abda6c6c9322bc468405cde96bed49
113114_RIVERA_IGNACIO <113114@tecnicatura.frc.utn.edu.ar>
2025-11-23 18:59:03 -0300

    Cambio url de api en environments

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api',
+    apiUrl: 'https://backend-production-a303.up.railway.app/api',
     imageKey: '8401d442af01af004be358cc69325230',
     firebaseConfig: {
         apiKey: "AIzaSyDyEQrGpRzEHjjD3mLEeFjNND7m22RXeqE",
